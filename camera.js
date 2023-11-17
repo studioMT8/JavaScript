20b5d19828a3e8dac3153c6e9e57770092e0247d
studioMT8 <95629475+studioMT8@users.noreply.github.com>
2023-11-17 18:37:44 +0900

    Update camera.js
    何やら盛大に間違ってたので更新

--- camera.js
+++ camera.js
@@ -1,47 +1,144 @@
 <script>
-  class canvas {
-    canvasID  = null;  //カンバスのhtmlElementID
-    canvasEle = null;  //カンバスのhtmlElement
+  class cameraDevice {
+    stream    = null;  //ビデオストリーム
+    finderID  = null;  //ファインダーのhtmlElementID
+    finderEle = null;  //ファインダーのhtmlElement
+    usingCamera = null; //使っているカメラ
 
-    /**
-     * カンバスの定義
-     * @param {HTMLElement} canvasElementID カンバスのエレメントID
-     */
-    open(canvasElementID) {
-      this.canvasID  = canvasElementID;
-      this.canvasEle = document.getElementById(canvasElementID);
-    }
+    /**
+     * カメラを使えるようにする
+     * @param {number} width 撮影する幅の画素数
+     * @param {number} height 撮影する高さの画素数
+     * @param {function} afterOpenCamera カメラ選択後に起動する関数
+     */
+    open(width ,height ,afterOpenCamera) {
+      /* カメラの選択 */
+        //リアカメラの仕様の定義
+      const rearCameraSpec = {
+        audio: false,
+        video: {
+          width : width  ,
+          height: height ,
+          facingMode: {exact: "environment"}
+        }
+      };
 
-    /**
-     * カンバスの大きさの指定
-     * @param {number} canvasWidth カンバスの横方向の画素数
-     * @param {number} canvasHeight カンバスの縦方向の画素数
-     * @param {number} showWidth 見た目の横方向の画素数
-     * @param {number} showHeight 見た目の縦方向の画素数
-     */
-    setDimensions(canvasWidth ,canvasHeight ,showWidth ,showHeight) {
-      //表示の大きさ
-      this.canvasEle.style.width  = showWidth  + "px";
-      this.canvasEle.style.height = showHeight + "px";
+      //フロントカメラの仕様の定義
+      const frontCameraSpec = {
+        audio: false,
+        video: {
+          width : width  ,
+          height: height ,
+          facingMode: "user"
+        }
+      };
 
-      //カンバス自体の大きさ
-      this.canvasEle.width  = canvasWidth;
-      this.canvasEle.height = canvasHeight;
-    }
+      if(this.usingCamera == null) {
+        //使っているカメラがなければ、どのカメラを使うかを判定する
+        navigator.mediaDevices.getUserMedia(rearCameraSpec)
+          .then((stream) => {
+            //リアカメラがあればそれを使う
+            this.stream = stream;
+            this.usingCamera = rearCameraSpec;
+            if(afterOpenCamera !== undefined) {
+              afterOpenCamera();
+            }
+          })
+          .catch((err) => {
+            //console.log(err.name + ": " + err.message + "　リアカメラなし");
 
-    /**
-     * 画像全域表示
-     * @param {HTMLElement} pictEleID 表示する画像があるエレメントID
-     * @param {number} showWidth 表示する幅
-     * @param {number} showHeight 表示する高さ
-     */
-    showImageFull(pictEleID ,showWidth ,showHeight) {
-      const pictEle = document.getElementById(pictEleID);
-      this.canvasEle.getContext("2d").clearRect(0, 0, this.canvasEle.width, this.canvasEle.height);
-      this.canvasEle.getContext("2d").drawImage(pictEleID,
-        0, 0, showWidth ,showHeight , //画像のどの範囲を表示するか
-        0, 0, this.canvasEle.width, this.canvasEle.height   //カンバスのどの範囲に表示するか
-      );
-    }
-  }
+            //リアカメラがなければフロントカメラを使う
+            navigator.mediaDevices.getUserMedia(frontCameraSpec)
+              .then((stream) => {
+                this.stream = stream;
+                this.usingCamera = frontCameraSpec;
+                if(afterOpenCamera !== undefined) {
+                  afterOpenCamera();
+                }
+              })
+            });
+      } else {
+        //リアカメラ、フロントカメラのいずれかが使われていたらそれを起動する
+        //カメラON
+        navigator.mediaDevices.getUserMedia(this.usingCamera)
+          .then((stream) => {
+            this.stream = stream;
+            if(afterOpenCamera !== undefined) {
+              afterOpenCamera();
+            }
+          })
+      }
+    }
+
+
+    /**
+     * ビデオストリームの取得
+     *
+     */
+    getStream() {
+      return this.stream;
+    }
+
+    getFinderID() {
+      return this.finderID;
+    }
+
+    getFinderEle() {
+      return this.finderEle;
+    }
+
+    /**
+     * カメラファインダーの表示
+     *
+     * @param {HTMLElement} finderElementID ファインダーのvideoエレメントID
+     * @param {number} finderWidth ファインダーの横方向の画素数
+     * @param {number} finderHeight ファインダーの縦方向の画素数
+     */
+    openFinder(finderElementID ,finderWidth ,finderHeight) {
+      this.finderID  = finderElementID;
+      this.finderEle = document.getElementById(finderElementID);
+
+      //ファインダーの画素数の指定
+      this.finderEle.style.width  = finderWidth  + "px";
+      this.finderEle.style.height = finderHeight + "px";
+
+      //ビデオストリームを取り出してカメラを表示
+      this.finderEle.srcObject = this.stream;
+      this.finderEle.onloadedmetadata = (e) => {
+        this.finderEle.play();
+      };
+    }
+
+
+    /**
+     * シャッターボタン押下
+     *
+     * @param {HTMLElement} takenPictureCanvasID 撮影した画像を表示するcanvasエレメントID
+     */
+    takePicture(takenPictureCanvasID) {
+      // 演出的な目的で一度映像を止めてSEを再生する
+      this.finderEle.pause();  // 映像を停止
+      //se.play();      // シャッター音
+      setTimeout( () => {
+        this.finderEle.play();    // 0.5秒後にカメラ再開
+      }, 500);
+    }
+
+
+    /***
+     * カメラファインダーを閉じる
+     */
+    closeFinder() {
+      /*
+      this.finderEle.srcObject.getTracks().forEach(function(track) {
+        track.stop();
+      });
+      */
+
+//      this.finderEle.pause();
+//      this.finderEle.stop();
+
+      this.finderEle.srcObject = null;
+    }
+  }
 </script>
