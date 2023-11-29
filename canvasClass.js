25a334a981e28bea8a0e423b5d7e5ed4fb688ad6
studioMT8 <95629475+studioMT8@users.noreply.github.com>
2023-11-29 10:47:28 +0900

    Update canvasClass.js

--- canvasClass.js
+++ canvasClass.js
@@ -1,47 +1,51 @@
 <script>
-  class canvas {
-    canvasID  = null;  //カンバスのhtmlElementID
-    canvasEle = null;  //カンバスのhtmlElement
+class canvas {
+  /**
+   * カンバスの定義
+   * @param {HTMLElement} canvasElementID カンバスのエレメントID
+   */
+  constructor(canvasElementID) {
+    this.canvasID  = canvasElementID;                           //カンバスのhtmlElementID
+    this.canvasEle = document.getElementById(canvasElementID);  //カンバスのhtmlElement
+  }
 
-    /**
-     * カンバスの定義
-     * @param {HTMLElement} canvasElementID カンバスのエレメントID
-     */
-    open(canvasElementID) {
-      this.canvasID  = canvasElementID;
-      this.canvasEle = document.getElementById(canvasElementID);
-    }
+  /**
+   * カンバスの大きさの指定
+   * @param {number} canvasWidth カンバスの横方向の画素数
+   * @param {number} canvasHeight カンバスの縦方向の画素数
+   * @param {number} showWidth 見た目の横方向の画素数
+   * @param {number} showHeight 見た目の縦方向の画素数
+   */
+  setDimensions(canvasWidth ,canvasHeight ,showWidth ,showHeight) {
+    //表示の大きさ
+    this.canvasEle.style.width  = showWidth  + "px";
+    this.canvasEle.style.height = showHeight + "px";
 
-    /**
-     * カンバスの大きさの指定
-     * @param {number} canvasWidth カンバスの横方向の画素数
-     * @param {number} canvasHeight カンバスの縦方向の画素数
-     * @param {number} showWidth 見た目の横方向の画素数
-     * @param {number} showHeight 見た目の縦方向の画素数
-     */
-    setDimensions(canvasWidth ,canvasHeight ,showWidth ,showHeight) {
-      //表示の大きさ
-      this.canvasEle.style.width  = showWidth  + "px";
-      this.canvasEle.style.height = showHeight + "px";
+    //カンバス自体の大きさ
+    this.canvasEle.width  = canvasWidth;
+    this.canvasEle.height = canvasHeight;
+  }
 
-      //カンバス自体の大きさ
-      this.canvasEle.width  = canvasWidth;
-      this.canvasEle.height = canvasHeight;
-    }
+  /**
+   * 画像全域表示
+   * @param {HTMLElement} pictEleID 表示する画像があるエレメントID
+   * @param {number} imgDispWidth 画像の表示幅
+   * @param {number} imgDispHeight 画像の表示高さ
+   */
+  showImageFull(pictEleID ,imgDispWidth ,imgDispHeight) {
+    const pictEle = document.getElementById(pictEleID);
+    this.clear();
+    this.canvasEle.getContext("2d").drawImage(pictEle,
+      0, 0, imgDispWidth ,imgDispHeight , //画像のどの範囲を表示するか
+      0, 0, this.canvasEle.width, this.canvasEle.height   //カンバスのどの範囲に表示するか
+    );
+  }
 
-    /**
-     * 画像全域表示
-     * @param {HTMLElement} pictEleID 表示する画像があるエレメントID
-     * @param {number} showWidth 表示する幅
-     * @param {number} showHeight 表示する高さ
-     */
-    showImageFull(pictEleID ,showWidth ,showHeight) {
-      const pictEle = document.getElementById(pictEleID);
-      this.canvasEle.getContext("2d").clearRect(0, 0, this.canvasEle.width, this.canvasEle.height);
-      this.canvasEle.getContext("2d").drawImage(pictEleID,
-        0, 0, showWidth ,showHeight , //画像のどの範囲を表示するか
-        0, 0, this.canvasEle.width, this.canvasEle.height   //カンバスのどの範囲に表示するか
-      );
-    }
-  }
+  /**
+   * 画像全域をクリア
+   */
+  clear() {
+    this.canvasEle.getContext("2d").clearRect(0, 0, this.canvasEle.width, this.canvasEle.height);
+  }
+}
 </script>
