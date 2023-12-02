d30a46af4f02546292a745db8e2a983dacdca48d
studioMT8 <95629475+studioMT8@users.noreply.github.com>
2023-12-02 19:36:32 +0900

    Update imagesClass.js

--- imagesClass.js
+++ imagesClass.js
@@ -1,76 +1,65 @@
 <script>
-class imageClass {
-  TYPE_FILE   = "FILE";    //<input type="file">で選択されたファイル
-  TYPE_SERVER = "SERVER";  //サーバにあるファイル
-  TYPE_GAS    = "GAS";    //GASの画像ファイル
-
+class image {
   /**
    * 画像の定義
    */
+
+  width;  //画像の幅の初期化
+  height;  //画像の高さの初期化
+  type;  //画像種別
+  loaded;  //ロード済みか（初期値：未ロード）
+  dispEleID;  //表示するエレメントID
+  imgObj;
+
   constructor() {
+    Object.defineProperty(this ,'TYPE_LOCAL'   ,{value: "LOCAL"});
+    Object.defineProperty(this ,'TYPE_SERVER' ,{value: "SERVER"});
+    Object.defineProperty(this ,'TYPE_GAS'    ,{value: "GAS"});
+
     this.width  = 0;  //画像の幅の初期化
     this.height = 0;  //画像の高さの初期化
     this.type = "";  //画像種別
     this.loaded = false;  //ロード済みか（初期値：未ロード）
     this.dispEleID = "";  //表示するエレメントID
-    this.image = new Image();
   }
 
   /**
-   * <input type="file">で指定された画像ファイルの属性を取り出す
+   * <input type="file">で指定された画像ファイルの表示と属性の取り出し
    * @param inputFileID <input type="file">のID
    * @param imgEleID 画像を表示するimgタグのID
    * @param nextFunc ファイルの属性を取り出した後に実行する関数
    */
-  getLocalImgFileAttr(inputFileID ,imgEleID ,nextFunc) {
+  localImgFile(inputFileID ,imgEleID ,nextFunc) {
     const ele = document.getElementById(inputFileID);
     if(ele.files[0]) {
-      this.image.src = URL.createObjectURL(ele.files[0]);
-
-      this.image.onload = function() {
-        this.width  = image.naturalWidth;
-        this.height = image.naturalHeight;
-        this.type   = TYPE_FILE;
+      const ID = document.getElementById(imgEleID);
+      ID.src = URL.createObjectURL(ele.files[0]);     //this.image.src;
+      ID.onload = function() {
+        this.width  = ID.naturalWidth;
+        this.height = ID.naturalHeight;
+        this.type   = this.TYPE_LOCAL;
         this.loaded = true;
-
-        if(imgEleID !== undefined) {
-          showLocalImgFile(imgEleID ,nextFunc);
+        this.dispEleID = imgEleID;
+        if(nextFunc !== undefined) {
+          nextFunc(this);
         }
       }
     }
   }
 
   /**
-   * <input type="file">で指定された画像ファイルを表示する
-   * @param imgEleID 画像を表示するimgタグのID
-   * @param nextFunc 画像を表示した後に実行する関数
-   */
-  showLocalImgFile(imgEleID ,nextFunc) {
-    const ID = document.getElementById(imgEleID);
-    ID.src = this.image.src;
-
-    ID.onload = function() {
-      this.dispEleID = imgEleID;
-      if(nextFunc !== undefined) {
-        nextFunc(this);
-      }
-    }
-  }
-
-
-  /**
-   * imgタグで表示された画像ファイルの属性を取り出す
+   * imgタグで表示された画像ファイルの属性の取り出し
    * @param imgEleID imgタグのID
    * @param nextFunc ファイルの属性を取り出した後に実行する関数
    */
-  getServerImgFileAttr(imgEleID ,nextFunc) {
-    const ID = document.getElementById(imgEleID);
-    this.image.src = ID.src;
-
-    this.image.onload = function() {
-      this.width  = image.naturalWidth;
-      this.height = image.naturalHeight;
-      this.type   = TYPE_SERVER;
+  serverImgFile(imgEleID ,nextFunc) {
+    var imgObj = new Image();
+    imgObj.src = document.getElementById(imgEleID).src;
+    this.imgObj = imgObj;
+    imgObj.onload = function() {
+      this.width  = imgObj.naturalWidth;
+      this.height = imgObj.naturalHeight;
+      this.type   = this.TYPE_SERVER;
       this.loaded = true;
       this.dispEleID = imgEleID;
       if(nextFunc !== undefined) {
@@ -79,41 +68,46 @@ class imageClass {
     }
   }
 
-
   /**
-   * GASの画像ファイルIDの内容をbase64で読み込む
+   * GASの画像ファイルIDの内容をbase64での読み込み
    * @param GASFileID GASの画像ファイルID
    * @param imgEleID base64で読み込んだ画像を表示するimgタグのID
    * @param nextFunc 画像を表示した後に実行する関数
    */
-  getGASImgFileAttr(GASFileID ,imgEleID ,nextFunc) {
-    const url = google.script.run
+  GASImgFile(GASFileID ,imgEleID ,nextFunc) {
+    var self = this;
+    google.script.run
       .withSuccessHandler(base64Data => {
-      this.image.src = "data:image/png;base64," + imgBase64;
+        var imgObj = new Image();
+        imgObj.src = "data:image/png;base64," + base64Data;
+        this.imgObj = imgObj;
 
-      this.image.onload = function() {
-        this.width  = image.naturalWidth;
-        this.height = image.naturalHeight;
-        this.type   = TYPE_GAS;
-        this.loaded = true;
+        imgObj.onload = function() {
+          this.width  = imgObj.naturalWidth;
+          this.height = imgObj.naturalHeight;
+          this.type   = this.TYPE_GAS;
+          this.loaded = true;
 
-        if(imgEleID !== undefined) {
-          this.showGASImgFile(imgEleID ,base64Data ,nextFunc);
+          //表示するエレメントIDが指定されていれば画像を表示する
+          if(imgEleID !== undefined) {
+            self.showGASImg(imgObj ,imgEleID ,nextFunc);
+          }
         }
       })
       .withFailureHandler(console.error)
-      .loadImgInBase64Format(GASFileID); 
+      .loadImgInBase64Format(GASFileID);
   }
 
   /**
-   * GASの画像ファイルの表示
-   * @param imgEleID 画像を表示するimgタグのID
-   * @param imgBase64 画像データ
+   * GASの画像の表示
+   * @param imgObj 画像オブジェクト
+   * @param imgEleID base64で読み込んだ画像を表示するimgタグのID
    * @param nextFunc 画像を表示した後に実行する関数
    */
-  showGASImgFile(imgEleID ,imgBase64 ,nextFunc) {
-    const ID = document.getElementById(imgID);
-    ID.src = "data:image/png;base64," + imgBase64);
+  showGASImg(imgObj ,imgEleID ,nextFunc) {
+    this.dispEleID = imgEleID;
+    const ID = document.getElementById(imgEleID);
+    ID.src = imgObj.src;
 
     ID.onload = function() {
       this.dispEleID = imgEleID;
@@ -121,86 +115,6 @@ class imageClass {
         nextFunc(this);
       }
     }
-  }
-}
-
-
-/////////////////////////////////////////////////////////////////////
-
-
-/**
- * <input type="file"> で指定された画像ファイルの属性を取り出す
- * @param {string} inputFileID <input type="file">のID
- * @param {function} nextFunc ファイルの属性を取り出した後に実行する関数
- */
-function getImgFileAttrAfterFileRead(inputFileID ,nextFunc) {
-  const ID = document.getElementById(inputFileID);
-  if(ID.files[0]) {
-    let image = new Image();
-    image.src = URL.createObjectURL(ID.files[0]);
-
-    image.onload = function() {
-      let result = {
-        width  : image.naturalWidth ,
-        height : image.naturalHeight
-      };
-
-      if(nextFunc !== undefined) {
-        nextFunc(result);
-      }
-    }
-  }
-}
-
-/**
- * imgタグで表示された画像ファイルの属性を取り出す
- * @param {string} imgID imgタグのID
- * @param {function} nextFunc ファイルの属性を取り出した後に実行する関数
- */
-function getImgFileAttrAfterFileLoaded(imgID ,nextFunc) {
-  const ID = document.getElementById(imgID);
-  let image = new Image();
-  image.src = ID.src;
-
-  let result = {
-    width  : image.naturalWidth ,
-    height : image.naturalHeight
-  };
-
-  if(nextFunc !== undefined) {
-    nextFunc(result);
-  }
-}
-
-/**
- * GASの画像ファイルIDの内容をbase64で読み込む
- * @param {string} GASFileID GASの画像ファイルID
- * @param {string} imgID imgタグのID
- * @param {function} nextFunc base64で読み込んだファイルの属性を取り出した後に実行する関数
- */
-function loadImgInBase64Image(GASFileID ,imgID ,nextFunc) {
-  const url = google.script.run
-    .withSuccessHandler(base64Data => {
-      if(nextFunc !== undefined) {
-        nextFunc(GASFileID ,imgID ,base64Data);
-      }
-    })
-    .withFailureHandler(console.error)
-    .loadImgInBase64Format(GASFileID); 
-}
-
-/**
- * GASの画像ファイルの表示
- * @param {string} imgID 表示するimgタグのID
- * @param {string} GASFileID GASの画像ファイルID
- * @param {bin} imgBase64 画像データ
- */
-function showGASImageFile(GASFileID ,imgID ,imgBase64) {
-  const ID = document.getElementById(imgID);
-  ID.setAttribute("src", "data:image/png;base64," + imgBase64);
-
-  ID.onload = function() {
-    getImgFileAttrAfterFileLoaded(imgID ,dispDimension);
   }
 }
 </script>
